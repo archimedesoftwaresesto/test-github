ad1d3354990a7d84190c24efb2e019c046b95263
pushpinderIU <pushpindersingh00906@gmail.com>
2024-06-12 15:54:35 +0200

    new_modification_rows

--- file1.py
+++ file1.py
@@ -1,3 +1,4 @@
+#001
 #first line
 print('commento****')
 print('Sono il file 1')
@@ -14,11 +15,14 @@ print('Sono il file 1')
 print('aggiungo una riga')
 print('aggiungo una riga bla bla')
 print('altro print')
-print('Faccio una modifica al file')print('Sono il file 1')
+#mofifica in mezzo
+print('Faccio una modifica al file')
+print('Sono il file 1')
 print('aggiungo una riga')
 print('aggiungo una riga bla bla')
 print('altro print')
-print('Faccio una modifica al file')print('Sono il file 1')
+print('Faccio una modifica al file')
+print('Sono il file 1')
 #modifica dario2
 print('aggiungo una riga')
 print('aggiungo una riga bla bla')
